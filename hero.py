7192a122396b3b112977e913b3c91273d3f8d879
Paul Fletes <paul.fletes@gmail.com>
2022-11-18 15:17:35 -0600

    update fight method with more conditions

--- hero.py
+++ hero.py
@@ -42,11 +42,12 @@ class Hero:
             total_defense += armor.block()
         return total_defense
 
-    def take_damage(self, damage):
+    def take_damage(self, name, damage):
         '''Updates self.current_health to reflect the damage minus the defense'''
         damage -= self.defend()
-        print(f'damage points is {damage}')
+        print(f'{name}\'s damage points is {damage}')
         self.current_health -= damage
+        # print(f'{name}\'s current health is {self.current_health})
 
     def is_alive(self):
         '''Return True or False depending on whether the hero is alive or not'''
@@ -60,10 +61,23 @@ class Hero:
         random_number = random.randint(0, 1)
         winner = ''
         loser = ''
-        if bool(self.abilities) or bool(opponent.abilities):
+        if bool(self.abilities) == False and bool(opponent.abilities) == False:
             print('draw')
         else:
-            print('fight')
+            while self.is_alive() == True and opponent.is_alive() == True:
+                opponent_damage = self.attack()
+                opponent.take_damage(opponent.name, opponent.damage)
+                self_damage = opponent.attack()
+                self.take_damage(self.name, self_damage)
+                if self.current_health > opponent.current_health:
+                    winner = self.name
+                    loser = opponent.name
+                else:
+                    winner = opponent.name
+                    loser = self.name
+                self.is_alive()
+                opponent.is_alive()
+            print(f'{winner} beats {loser}')
         return winner
 
 
