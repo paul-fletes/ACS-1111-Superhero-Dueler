0d1eaee87b228c3ba339167ef12cde601039ebc9
Paul Fletes <paul.fletes@gmail.com>
2022-11-18 16:54:22 -0600

    create weapon class and add attack method

---
+++ weapon.py
@@ -0,0 +1,14 @@
+from ability import Ability
+import random
+
+
+class Weapon(Ability):
+    '''
+    This method returns a random value
+    between one half to the full attack power of the weapon
+    '''
+
+    def attack(self):
+        divided_number = self.max_damage / 2
+        random_int = random.randint(divided_number, self.max_damage)
+        return random_int
